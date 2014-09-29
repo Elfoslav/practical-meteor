38de49d5c030f0a7b0aae2fa9785855be3266969
Tomáš Hromník <tom.hromnik@gmail.com>
2014-09-29 14:30:26 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Glorify Meteor!
+===
+
+Runnable Meteor.js examples
+
+[glorify.meteor.com](http://glorify.meteor.com)
---
+++ glorify-meteor.js
@@ -0,0 +1,24 @@
+//default Meteor example
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault("counter", 0);
+
+  Template.home.helpers({
+    counter: function () {
+      return Session.get("counter");
+    }
+  });
+
+  Template.home.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set("counter", Session.get("counter") + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}

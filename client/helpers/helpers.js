4baf06e366cd817a5023d01a21b09b2020c03936
Tomáš Hromník <tom.hromnik@gmail.com>
2014-10-14 15:14:42 +0200

    Helpers lesson

--- client/helpers/helpers.js
+++ client/helpers/helpers.js
@@ -4,6 +4,29 @@ Template.helpers.rendered = function() {
 
 Template.helpers.helpers({
   books: function() {
-    return Books.find();
+    return ['Discover Meteor', 'Meteor in action', 'Practical Meteor'];
+  },
+  booksObjects: function() {
+    return [
+      { title: 'Discover Meteor', pages: 300 },
+      { title: 'Meteor in action', pages: 250 },
+      { title: 'Practical Meteor', pages: 199 }
+    ];
+  },
+  add: function(a, b) {
+    return a + b;
+  },
+  showBooks: function() {
+    return '{{#each books}}\n' +
+            '  <p>{{this}}</p>\n' +
+            '{{/each}}';
+  },
+  showBooksObjects: function() {
+    return '{{#each books}}\n' +
+            '  <p>{{title}}, {{pages}} pages.</p>\n' +
+            '{{/each}}';
+  },
+  addHelper: function() {
+    return '{{add 5 6}}';
   }
 });

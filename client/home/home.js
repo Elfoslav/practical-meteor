8b49fb9bde752995614aa686dca31293c19829eb
Tomáš Hromník <tom.hromnik@gmail.com>
2014-10-16 12:33:11 +0200

    Reworked Intro according to default Meteor template

--- client/home/home.js
+++ client/home/home.js
@@ -1,3 +1,26 @@
 Template.home.rendered = function() {
   SyntaxHighlight();
 };
+
+// counter starts at 0
+Session.setDefault("counter", 0);
+
+Template.home.helpers({
+  counter: function () {
+    return Session.get("counter");
+  },
+  templateExample: function() {
+    return '' +
+    '<template name="hello">\n' +
+    '  <button>Click me</button>\n' +
+    '  <p>You\'ve pressed the button {{counter}} times.\n' +
+    '</template>';
+  }
+});
+
+Template.home.events({
+  'click button': function () {
+    // increment the counter when button is clicked
+    Session.set("counter", Session.get("counter") + 1);
+  }
+});
--- glorify-meteor.js
+++ glorify-meteor.js
@@ -1,27 +1,6 @@
 //default Meteor example
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.home.helpers({
-    counter: function () {
-      return Session.get("counter");
-    },
-    templateExample: function() {
-      return '' +
-      '<template name="home">\n' +
-        ' <button>Click me</button>\n' +
-        ' <p>Click the button above {{counter}}</p>\n' +
-      '</template>';
-    }
-  });
-
-  Template.home.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+  //Client code
 }
 
 if (Meteor.isServer) {
